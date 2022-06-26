c01df8f0c9b58fc14745871af08defcafe2230da
pavelsjo <pavels.jo@gmail.com>
2022-06-26 15:59:59 -0300

    First commit

---
+++ 2-entorno-para-dibujar/sketch-01.js
@@ -0,0 +1,43 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+    context.lineWidth = width * 0.01;
+
+    const w = width * 0.10;
+    const h = width * 0.10;
+    const gap = width * 0.03;
+    const ix = width * 0.17;
+    const iy = height * 0.17;
+
+    const off = width * 0.02;
+    let x, y;
+
+    for(let i = 0; i < 5; i++) {
+      for(let j = 0; j < 5; j++) {
+        let x = ix + (w + gap) * i;
+        let y = iy + (h + gap) * j;
+    
+        context.beginPath();
+
+        context.rect(x, y, w, h);
+        context.stroke();
+        
+        if(Math.random() > 0.5) {
+          context.strokeStyle = 'white';          context.beginPath();
+          
+          context.rect(x + off/2, y + off/2, w - off, h - off);
+          context.stroke();
+        }
+      }
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
