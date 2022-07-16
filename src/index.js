fbdb89ed50d73c399b7c0c3bf30c48f70e979b58
pavelsjo <pavels.jo@gmail.com>
2022-07-16 15:42:29 -0300

    New static version

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-let room = document.querySelector('.room');
+const images = document.images;
+const frame = document.getElementById('frame');
 
-const show = () => {
-    for (let i = 0; i < 3; i++) {
-        const canvas = drawMesh(400, 400);
-        room.appendChild(canvas);
+const draw = () => {
+
+    const [width, height] = [300, 300]
+
+    for(let i = 0; i < images.length; i++) {
+        if(images[i].getAttribute('id') !== 'frame') {
+            const canvas = document.createElement('canvas');
+
+            // setup
+            canvas.setAttribute('width', width);
+            canvas.setAttribute('height', height);
+
+            images[i].parentNode.insertBefore(canvas, images[i]);
+
+            const context = canvas.getContext('2d');
+
+            // draw
+            context.drawImage(images[i], 0, 0, 1000, 1000, 10, 10, 250, 250);
+            context.drawImage(frame, 0, 0, 270, 270);
+
+        }
     }
 }
-
-show();
-
-
